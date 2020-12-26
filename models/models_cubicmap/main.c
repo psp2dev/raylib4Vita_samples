271d0257eb24e542fdc48df49f3f435e6e5becd7
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2020-12-26 16:37:10 +0100

    vitasdk compliant

--- models/models_cubicmap/main.c
+++ models/models_cubicmap/main.c
@@ -18,6 +18,7 @@
 #include <psp2/gxm.h>
 #include <psp2/ctrl.h>
 #include <psp2/system_param.h>
+extern uint32_t sceLibcHeapSize = 2194304;
 bool flag=true;
 
 #define ALIGN_CENTER(a, b) (((a) - (b)) / 2)
@@ -149,7 +150,7 @@ void readPad() {
     }
   }
 
-  if (enter_button == SCE_SYSTEM_PARAM_ENTER_BUTTON_ASSIGN_CIRCLE) {
+  if (enter_button == SCE_SYSTEM_PARAM_ENTER_BUTTON_CIRCLE) {
     old_pad[PAD_ENTER] = old_pad[PAD_CIRCLE];
     current_pad[PAD_ENTER] = current_pad[PAD_CIRCLE];
     pressed_pad[PAD_ENTER] = pressed_pad[PAD_CIRCLE];
